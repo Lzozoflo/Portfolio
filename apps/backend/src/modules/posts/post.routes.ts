3cfd6d8895ccc44721a1bdb7ac0e0a34afa51638
Florent Cretin <florent.cretin@hotmail.fr>
2026-03-28 13:34:49 +0100

    base a parse pour le site web

--- apps/backend/src/modules/posts/post.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Router } from 'express';
-
-export const postsRouter = Router();
-
-// TODO: implémenter le module posts
-postsRouter.get('/', (_req, res) => res.json({ data: [] }));
--- apps/backend/src/modules/users/user.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Router } from 'express';
-
-export const usersRouter = Router();
-
-// TODO: brancher userController + validate
-usersRouter.get('/', (_req, res) => res.json({ data: [] }));
