637e4eaa53f617c2002b125a8c18a349f8de9225
Florent Cretin <florent.cretin@hotmail.fr>
2026-04-02 11:17:53 +0200

    codemirror we can edit file so mow i have to save it

--- apps/frontend/src/Route/Home/Inviter/Inviter.tsx
+++ apps/frontend/src/Route/Home/Inviter/Inviter.tsx
@@ -1,9 +1,9 @@
 /* extern */
-import { useEffect, useState } from "react";
-
-
-/* back */
-
+import { useEffect, useState, useMemo } from "react";
+import CodeMirror from '@uiw/react-codemirror';
+// import { javascript } from '@codemirror/lang-javascript';
+// import { markdown } from '@codemirror/lang-markdown';
+// import { oneDark } from '@codemirror/theme-one-dark';
 
 /* Css */
 import './Inviter.scss'
@@ -12,25 +12,37 @@ import './Inviter.scss'
 import Hr from 'COMP/Hr/Hr'
 import Explorateur from "./Explorateur/Explorateur";
 
-
 /* Types */
 import type { FileNode, IDBNode } from '@portfolio/shared';
 
-//interface InviterProps {
-//    children: ReactNode;
-//    className?: string;
-//}
+interface InviterProps { 
+    fileSystem: FileNode | undefined,
+    crud: any 
+}
 
-export default function Inviter({ fileSystem, crud}: { fileSystem: FileNode | undefined , crud: any }) {
-
+export default function Inviter({ fileSystem, crud }: InviterProps) {
     const [fileToDisplay, setFileToDisplay] = useState<IDBNode | undefined>(undefined);
+    const [content, setContent] = useState<string | null>(null);
 
     async function handelscreen(pwd: string) {
-        // console.log("handelscreen: ",pwd);
-        const resCrudCat = await crud.cat(pwd); 
-        console.log("resCrudCat:",resCrudCat);
+        const resCrudCat = await crud.cat(pwd);
+        console.log("resCrudCat:",resCrudCat,"fileToDisplay:",fileToDisplay);
+        
+        if (resCrudCat.path === fileToDisplay?.path){
+            console.log("test");
+            setFileToDisplay(undefined);
+            return;
+        }
         setFileToDisplay(resCrudCat);
     }
+
+    useEffect(() => {
+        if (fileToDisplay === undefined){
+            setContent(null)
+            return;
+        } 
+        setContent(fileToDisplay?.data ?? "");
+    }, [fileToDisplay]);
 
     return (
         <div className={`Inviter-root`}>
@@ -47,55 +59,29 @@ export default function Inviter({ fileSystem, crud}: { fileSystem: FileNode | un
                     <button className={`btc-reset`} onClick={crud.resetDatabase}>reset</button>
                 </div>
 
-        {/* ─── <Hr> vertical </Hr> ───────────────────────────────────────────────── */}
-
                 <div className={`display`}>
-                    
                     <div className={`nav-bar`}>
 
                     </div>
 
                     <div className={`display-file`}>
-                        <p  contentEditable={true}
-                            style={{ whiteSpace: "pre-wrap" }}
-                            onBlur={(e) => console.log(e.target.innerText)}
-                        >
-                            {fileToDisplay?.data}
-                        </p>
-
+                        {content !== null && (
+                            <CodeMirror
+                                value={content}
+                                height="100%"
+                                // extensions={extensions}
+                                onChange={(value) => {setContent(value); console.log(`value:${value}`);}}
+                                className="codemirror-container"
+                                basicSetup={{
+                                    lineNumbers: true,
+                                    foldGutter: true,
+                                    highlightActiveLine: true,
+                                }}
+                            />
+                        )}
                     </div>
-
                 </div>
             </Hr>
         </div>
     )
 }
-
-
-
-//                 {/* frontend
-
-//                 React
-//                 JavaScript/TypeScript
-//                 Scss
-
-
-//                 backend
-
-//                 Node.js
-//                 Express.js
-
-//                 database
-
-//                 MySQL
-//                 sequalize
-
-//                 PostgreSQL
-//                 Prisma
-
-//                 tools
-//                 man zshmisc
-//                 Shell
-//                 Git
-//                 Docker */}
-//             </div>
