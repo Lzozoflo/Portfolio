94276c88791094b4927d42045720358c03d7bd97
Lzozoflo <florent.cretin@hotmail.fr>
2026-04-19 16:47:46 +0200

    upgrade view mobile better

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -22,6 +22,22 @@ export default function App() {
             setAuthLevel(authStep.PAGE_LOGIN); // TODO FAIRE UN checkTokenAuth(token)
         }
 
+        const handleResize = () => {
+            // On calcule 1% de la hauteur réelle visible
+            const vh = window.innerHeight * 0.01;
+            const wh = window.innerWidth * 0.01;
+            // On l'injecte dans le style de l'élément racine
+            document.documentElement.style.setProperty('--vh', `${vh}px`);
+            document.documentElement.style.setProperty('--wh', `${wh}px`);
+        };
+
+        handleResize();
+
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
     }, [])
 
     return (
